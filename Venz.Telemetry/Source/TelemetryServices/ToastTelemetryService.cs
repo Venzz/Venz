46944b3f47bc62905f54808474854447693ea93f
Venexz <venexzz@gmail.com>
2017-06-04 19:37:04 +0300

    Added ToastTelemetryService.

---
+++ Venz.Telemetry/Source/TelemetryServices/ToastTelemetryService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Windows.Data.Xml.Dom;
+using Windows.Foundation;
+using Windows.UI.Notifications;
+
+namespace Venz.Telemetry
+{
+    public sealed class ToastTelemetryService: ITelemetryService
+    {
+        private ToastNotifier Notifier = ToastNotificationManager.CreateToastNotifier();
+
+        public void Start()
+        {
+            ShowToast("TELEMETRY", "Application Launched");
+        }
+
+        public void Finish()
+        {
+            ShowToast("TELEMETRY", "Application Exit");
+        }
+
+        public void LogEvent(String title)
+        {
+            ShowToast("TELEMETRY", title);
+        }
+
+        public void LogEvent(String title, String parameter, String value)
+        {
+            ShowToast($"TELEMETRY >> {title}", $"{parameter}: {value}");
+        }
+
+        public void LogException(String comment, Exception exception)
+        {
+            ShowToast($"TELEMETRY >> {comment}", $"{exception.GetType().FullName}: {exception.Message}");
+        }
+
+        public IAsyncAction LogDailyEventAsync(String title)
+        {
+            LogEvent(title);
+            return TaskExtensions.CompletedAction;
+        }
+
+        public IAsyncAction LogEventAsync(String title)
+        {
+            LogEvent(title);
+            return TaskExtensions.CompletedAction;
+        }
+
+        public IAsyncAction LogEventAsync(String title, String parameter, String parameterValue)
+        {
+            LogEvent(title, parameter, parameterValue);
+            return TaskExtensions.CompletedAction;
+        }
+
+
+
+        private void ShowToast(String line1, String line2)
+        {
+            String normalize(String message, Int32 maxSize) => ((message.Length > maxSize) ? message.Remove(maxSize) : message).Replace("&", "&amp;").Replace("<", "&lt;").Replace(">", "&gt;").Replace("\"", "&quot;").Replace("'", "&apos;");
+            var template =
+                $@"<toast>
+                    <visual>
+                        <binding template='{ToastTemplateType.ToastText02}'>
+                            <text id='1'>{normalize(line1, 50)}</text>
+                            <text id='2'>{normalize(line2, 100)}</text>
+                        </binding>
+                    </visual>
+                </toast>";
+
+            var document = new XmlDocument();
+            document.LoadXml(template);
+            Notifier.Show(new ToastNotification(document));
+        }
+    }
+}
