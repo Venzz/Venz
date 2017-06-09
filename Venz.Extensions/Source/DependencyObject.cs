a1139d93681c38918e88b9d90008dbd73995dab0
Venexzz <venexzz@gmail.com>
2017-06-09 14:10:19 +0300

    Added TryFindVisualTreeChildren to help in creating some UI scenarios.

--- Venz.Extensions/Source/DependencyObject.cs
+++ Venz.Extensions/Source/DependencyObject.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Media;
 
@@ -23,6 +24,39 @@ namespace Venz.Extensions
 
             child = VisualTreeHelper.GetChild(child, (Int32)position);
             return (child is T) ? (T)child : null;
+        }
+
+        public static IReadOnlyCollection<T> TryFindVisualTreeChildren<T>(this DependencyObject source) where T: DependencyObject
+        {
+            return TryFindVisualTreeChildren<T>(source, null, null);
+        }
+
+        public static IReadOnlyCollection<T> TryFindVisualTreeChildren<T>(this DependencyObject source, DependencyProperty property, Object value) where T: DependencyObject
+        {
+            var children = new List<T>();
+            if (source == null)
+                return children;
+
+            var childCount = VisualTreeHelper.GetChildrenCount(source);
+            for (var i = 0; i < childCount; i++)
+            {
+                var sourceChild = VisualTreeHelper.GetChild(source, i);
+                if ((sourceChild is T) && ((property == null) || Object.Equals(((T)sourceChild).GetValue(property), value)))
+                {
+                    children.Add((T)sourceChild);
+                }
+                else
+                {
+                    if ((sourceChild is Windows.UI.Xaml.Controls.ContentPresenter) && (((Windows.UI.Xaml.Controls.ContentPresenter)sourceChild).Content is DependencyObject))
+                    {
+                        sourceChild = (DependencyObject)((Windows.UI.Xaml.Controls.ContentPresenter)sourceChild).Content;
+                        if (sourceChild is T)
+                            children.Add((T)sourceChild);
+                    }
+                    children.AddRange(TryFindVisualTreeChildren<T>(sourceChild, property, value));
+                }
+            }
+            return children;
         }
     }
 }
