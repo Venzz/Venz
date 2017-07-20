2f9bec01ac2680fecb69af7b3e2ebfe0309cbbac
Venzz <venexzz@gmail.com>
2017-07-20 17:19:24 +0300

    Added MenuHeader and MenuItem for SplitView control.

---
+++ Venz.UI.Xaml.Controls/Source/UI/Xaml/Controls/SplitViewMenuHeader.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Venz.UI.Xaml.Controls
+{
+    public sealed partial class SplitViewMenuHeader: UserControl
+    {
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(String), typeof(SplitViewMenuHeader),
+            new PropertyMetadata("", (obj, args) => ((SplitViewMenuHeader)obj).OnTextChanged((String)args.NewValue)));
+
+        public String Text
+        {
+            get => (String)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+        
+
+
+        public SplitViewMenuHeader()
+        {
+            InitializeComponent();
+        }
+
+        private void OnTextChanged(String value) => TextControl.Text = value.ToUpper();
+    }
+}
---
+++ Venz.UI.Xaml.Controls/Source/UI/Xaml/Controls/SplitViewMenuItem.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Venz.UI.Xaml.Controls
+{
+    public sealed partial class SplitViewMenuItem: UserControl
+    {
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(IconElement), typeof(SplitViewMenuItem), new PropertyMetadata(null));
+
+        public IconElement Icon
+        {
+            get => (IconElement)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(String), typeof(SplitViewMenuItem), new PropertyMetadata(""));
+
+        public String Text
+        {
+            get => (String)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+        
+
+
+        public SplitViewMenuItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
