4b628dcd1cf3dd7e809bde1c809c8a22446c46b5
Venzz <venexzz@gmail.com>
2017-10-14 17:07:52 +0300

    Added incrementally loaded collection.

---
+++ Venz.Core/Source/Data/IncrementalLoadingCollection.Interface.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Windows.Foundation;
+
+namespace Venz.Data
+{
+    public enum IncrementalLoadingState { Normal, Loading, WaitingForUserInput, Finished }
+
+    public enum IncrementalLoadingTriggerLocation { Bottom, Top }
+
+    public class LoadedItemsReport
+    {
+        public Boolean Final { get; private set; }
+        public Boolean Failed { get; private set; }
+
+        private LoadedItemsReport() { }
+        public static LoadedItemsReport CreateSuccessful(Boolean final) => new LoadedItemsReport() { Final = final };
+        public static LoadedItemsReport CreateFailed() => new LoadedItemsReport() { Failed = true };
+    }
+
+    public interface ISupportIncrementalLoading
+    {
+        Boolean HasMoreItems { get; }
+        IncrementalLoadingTriggerLocation TriggerLocation { get; }
+
+        event TypedEventHandler<ISupportIncrementalLoading, UInt32> MoreItemsRequested;
+        event TypedEventHandler<ISupportIncrementalLoading, IncrementalLoadingState> StateChanged;
+
+        void RequestMoreItems(UInt32 count);
+        void RetryRequestMoreItems();
+    }
+}
---
+++ Venz.Core/Source/Data/IncrementalLoadingCollection.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.Foundation;
+
+namespace Venz.Data
+{
+    public class IncrementalLoadingCollection<T>: ObservableList<T>, ISupportIncrementalLoading
+    {
+        private UInt32? RequestedItemsCount;
+
+        public Boolean HasMoreItems { get; private set; } = true;
+        public IncrementalLoadingTriggerLocation TriggerLocation { get; set; }
+
+        public event TypedEventHandler<ISupportIncrementalLoading, UInt32> MoreItemsRequested = delegate { };
+        public event TypedEventHandler<ISupportIncrementalLoading, IncrementalLoadingState> StateChanged = delegate { };
+
+
+
+        public IncrementalLoadingCollection() { }
+
+        public IncrementalLoadingCollection(IEnumerable<T> initialItems): base(initialItems) { }
+
+        public void RequestMoreItems(UInt32 count)
+        {
+            lock (SyncRoot)
+            {
+                if (RequestedItemsCount.HasValue)
+                    return;
+
+                RequestedItemsCount = count;
+                MoreItemsRequested(this, count);
+                StateChanged(this, IncrementalLoadingState.Loading);
+            }
+        }
+
+        public void RetryRequestMoreItems()
+        {
+            lock (SyncRoot)
+            {
+                if (RequestedItemsCount.HasValue)
+                {
+                    MoreItemsRequested(this, RequestedItemsCount.Value);
+                    StateChanged(this, IncrementalLoadingState.Loading);
+                }
+            }
+        }
+
+        public void ReportLoadedItems(LoadedItemsReport loadedItemsReport)
+        {
+            lock (SyncRoot)
+            {
+                if (loadedItemsReport.Failed)
+                {
+                    StateChanged(this, IncrementalLoadingState.WaitingForUserInput);
+                    return;
+                }
+
+                if (HasMoreItems)
+                    HasMoreItems = !loadedItemsReport.Final;
+
+                RequestedItemsCount = null;
+                StateChanged(this, HasMoreItems ? IncrementalLoadingState.Normal : IncrementalLoadingState.Finished);
+            }
+        }
+    }
+}
