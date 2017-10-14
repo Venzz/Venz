e308ce1ef4c933f017abd9d8b45609223988b833
Venzz <venexzz@gmail.com>
2017-10-14 17:08:28 +0300

    Added button.

---
+++ Venz.UI.Xaml.Controls/Source/UI/Xaml/Controls/Button.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Windows.Foundation;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+
+namespace Venz.UI.Xaml.Controls
+{
+    public class Button: global::Windows.UI.Xaml.Controls.Button
+    {
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register("Label", typeof(String), typeof(Button),
+            new PropertyMetadata(null, (obj, args) => ((Button)obj).OnLabelChanged((String)args.NewValue)));
+
+        public String Label
+        {
+            get => (String)GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+
+        public static readonly DependencyProperty CursorProperty =
+            DependencyProperty.Register("Cursor", typeof(CoreCursorType), typeof(Button), new PropertyMetadata(CoreCursorType.Arrow));
+
+        public CoreCursorType Cursor
+        {
+            get => (CoreCursorType)GetValue(CursorProperty);
+            set => SetValue(CursorProperty, value);
+        }
+
+        public static readonly DependencyProperty AvailableWidthProperty =
+            DependencyProperty.Register("AvailableWidth", typeof(Double), typeof(Button), new PropertyMetadata(720.0));
+
+        public Double AvailableWidth
+        {
+            get => (Double)GetValue(AvailableWidthProperty);
+            set => SetValue(AvailableWidthProperty, value);
+        }
+
+
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            if (GetTemplateChild("RootGrid") is global::Windows.UI.Xaml.Controls.Grid root)
+            {
+                var visualStateGroups = VisualStateManager.GetVisualStateGroups(root);
+                if (visualStateGroups.Count > 0)
+                    visualStateGroups[0].CurrentStateChanged += OnVisualStateChanged;
+            }
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            if (Double.IsInfinity(availableSize.Width))
+            {
+                return base.MeasureOverride(availableSize);
+            }
+            else
+            {
+                var defaultSize = base.MeasureOverride(availableSize);
+                return new Size((availableSize.Width < AvailableWidth) ? (availableSize.Width / 2 - 12) : AvailableWidth / 2, defaultSize.Height);
+            }
+        }
+
+        private void OnVisualStateChanged(Object sender, VisualStateChangedEventArgs args)
+        {
+            if ((args.NewState?.Name == "PointerOver") && (Cursor != CoreCursorType.Arrow))
+                Window.Current.CoreWindow.PointerCursor = new CoreCursor(Cursor, 1);
+        }
+
+        private void OnLabelChanged(String value) => Content = value;
+    }
+}
