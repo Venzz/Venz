66ada2aa43ae8f3cb753ae7bc8f4cd967ed618ed
Venzz <venexzz@gmail.com>
2018-05-20 15:45:05 +0300

    Added PasswordBox extension that allows get password changes immediately.

---
+++ Venz.UI.Xaml.Controls/Source/UI/Xaml/ControlsExtensions/PasswordBox.UpdatablePassword.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Windows.UI.Xaml;
+
+namespace Venz.UI.Xaml.Controls
+{
+    public static class PasswordBox
+    {
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.RegisterAttached("Password", typeof(String), typeof(PasswordBox),
+            new PropertyMetadata(Byte.MaxValue, (obj, args) => OnPasswordChanged((global::Windows.UI.Xaml.Controls.PasswordBox)obj, (String)args.NewValue)));
+
+        public static String GetPassword(DependencyObject obj) => (String)obj.GetValue(PasswordProperty);
+
+        public static void SetPassword(DependencyObject obj, String value) => obj.SetValue(PasswordProperty, value);
+
+        private static void OnPasswordChanged(global::Windows.UI.Xaml.Controls.PasswordBox element, String password)
+        {
+            element.Unloaded -= OnElementUnloaded;
+            element.Unloaded += OnElementUnloaded;
+            element.PasswordChanged -= OnPasswordBoxPasswordChanged;
+            element.Password = password;
+            element.PasswordChanged += OnPasswordBoxPasswordChanged;
+        }
+
+        private static void OnPasswordBoxPasswordChanged(Object sender, RoutedEventArgs args)
+        {
+            var passwordBox = (global::Windows.UI.Xaml.Controls.PasswordBox)sender;
+            SetPassword(passwordBox, passwordBox.Password);
+        }
+
+        private static void OnElementUnloaded(Object sender, RoutedEventArgs args)
+        {
+            var passwordBox = (global::Windows.UI.Xaml.Controls.PasswordBox)sender;
+            passwordBox.PasswordChanged -= OnPasswordBoxPasswordChanged;
+        }
+    }
+}
