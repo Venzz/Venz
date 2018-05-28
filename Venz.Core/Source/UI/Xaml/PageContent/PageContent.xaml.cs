ff67fad2b1f154761651d9e4839570750a537f83
Venzz <venexzz@gmail.com>
2018-05-28 22:14:24 +0300

    Added notifications layer to PageContent.

--- Venz.Core/Source/UI/Xaml/PageContent/PageContent.xaml.cs
+++ Venz.Core/Source/UI/Xaml/PageContent/PageContent.xaml.cs
@@ -5,7 +5,20 @@ namespace Venz.UI.Xaml
 {
     public class PageContent: ContentControl
     {
+        private Grid NotificationsLayerControl;
         private Grid CustomContentLayerControl;
+
+        public static readonly DependencyProperty NotificationsLayerContentProperty =
+            DependencyProperty.Register(nameof(NotificationsLayerContent), typeof(UIElement), typeof(PageContent),
+            new PropertyMetadata(null, (obj, args) => ((PageContent)obj).OnNotificationsLayerContentChanged((UIElement)args.NewValue)));
+
+        public UIElement NotificationsLayerContent
+        {
+            get => (UIElement)GetValue(NotificationsLayerContentProperty);
+            set => SetValue(NotificationsLayerContentProperty, value);
+        }
+
+
 
         public PageContent()
         {
@@ -17,7 +30,21 @@ namespace Venz.UI.Xaml
         protected override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
+            NotificationsLayerControl = (Grid)GetTemplateChild(nameof(NotificationsLayerControl));
             CustomContentLayerControl = (Grid)GetTemplateChild(nameof(CustomContentLayerControl));
+            if ((NotificationsLayerControl != null) && (NotificationsLayerContent != null))
+                NotificationsLayerControl.Children.Add(NotificationsLayerContent);
+        }
+
+        private void OnNotificationsLayerContentChanged(UIElement value)
+        {
+            if (NotificationsLayerControl == null)
+                return;
+
+            if (NotificationsLayerControl.Children.Count > 1)
+                NotificationsLayerControl.Children.RemoveAt(1);
+            if (value != null)
+                NotificationsLayerControl.Children.Add(value);
         }
     }
 }
