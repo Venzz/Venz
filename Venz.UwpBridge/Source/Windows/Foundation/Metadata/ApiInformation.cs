ddf406510b736aa111807704c3e8789d20f0dccd
Venexzz <venexzz@gmail.com>
2018-05-29 13:27:26 +0300

    Added Venz.UwpBridge project.

---
+++ Venz.UwpBridge/Source/Windows/Foundation/Metadata/ApiInformation.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Windows.Foundation.Metadata
+{
+    public static class ApiInformation
+    {
+        public static Boolean IsApiContractPresent(String contractName, UInt16 majorVersion, UInt16 minorVersion) => false;
+    }
+}
