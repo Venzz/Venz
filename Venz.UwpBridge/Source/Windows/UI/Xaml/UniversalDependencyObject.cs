03e14e2eecb5fbc05cf1bc13ba8b0e33fbf5b7a6
Venexzz <venexzz@gmail.com>
2018-05-30 17:23:01 +0300

    Added DependecyObject extensions to UwpBridge.

---
+++ Venz.UwpBridge/Source/Windows/UI/Xaml/UniversalDependencyObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Windows.UI.Xaml
+{
+    public class UniversalDependencyObject
+    {
+        public static Int64 RegisterPropertyChangedCallback(DependencyObject dependencyObject, DependencyProperty dp, String dpName, DependencyPropertyChangedCallback callback)
+        {
+            DependencyPropertyChangesListener.Create(dependencyObject, dp, dpName, callback);
+            return 0;
+        }
+
+        public delegate void DependencyPropertyChangedCallback(DependencyObject sender, DependencyProperty dp);
+
+        private class DependencyPropertyChangesListener: DependencyObject, IDisposable
+        {
+            private DependencyObject Target;
+            private DependencyProperty Property;
+            private DependencyPropertyChangedCallback Callback;
+
+            public static readonly DependencyProperty ValueProperty =
+                DependencyProperty.Register("Value", typeof(Object), typeof(DependencyObject),
+                new PropertyMetadata(null, (obj, args) => ((DependencyPropertyChangesListener)obj).OnValueChanged()));
+
+
+
+            private DependencyPropertyChangesListener() { }
+
+            public static void Create(DependencyObject target, DependencyProperty property, String propertyName, DependencyPropertyChangedCallback callback)
+            {
+                var listener = new DependencyPropertyChangesListener();
+                listener.Target = target;
+                listener.Property = property;
+                listener.Callback = callback;
+
+                var binding = new Data.Binding();
+                binding.Source = target;
+                binding.Mode = Data.BindingMode.OneWay;
+                binding.Path = new PropertyPath(propertyName);
+                Data.BindingOperations.SetBinding(listener, ValueProperty, binding);
+            }
+
+            private void OnValueChanged() => Callback.Invoke(Target, Property);
+
+            public void Dispose() => ClearValue(ValueProperty);
+        }
+    }
+}
