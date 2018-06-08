a29fcb4e34631b3ba3e74a0361fb8d10133b4628
Venzz <venexzz@gmail.com>
2018-06-08 14:03:34 +0300

    Added control extensions.

---
+++ Venz.UwpBridge/Source/System/Text/Encoding.cs
@@ -0,0 +1,10 @@
+﻿namespace System.Text
+{
+    public static class EncodingExtensions
+    {
+        public static String GetString(this Encoding encoding, Byte[] data)
+        {
+            return encoding.GetString(data, 0, data.Length);
+        }
+    }
+}
---
+++ Venz.UwpBridge/Source/Windows/UI/Xaml/Controls/MapControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls.Maps;
+
+namespace Windows.UI.Xaml.Controls
+{
+    public static class MapControlExtensions
+    {
+        public static IAsyncOperation<Boolean> TryZoomInAsync(this MapControl mapControl)
+        {
+            return mapControl.TrySetViewAsync(mapControl.Center, mapControl.ZoomLevel + 1);
+        }
+
+        public static IAsyncOperation<Boolean> TryZoomOutAsync(this MapControl mapControl)
+        {
+            return mapControl.TrySetViewAsync(mapControl.Center, mapControl.ZoomLevel - 1);
+        }
+    }
+}
