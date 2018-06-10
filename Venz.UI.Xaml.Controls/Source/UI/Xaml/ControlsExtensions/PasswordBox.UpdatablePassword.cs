92e6dc96f1316ae4cfd58de793473977981cc07e
Venzz <venexzz@gmail.com>
2018-06-10 17:39:57 +0300

    Fixed PasswordBox extension.

--- Venz.UI.Xaml.Controls/Source/UI/Xaml/ControlsExtensions/PasswordBox.UpdatablePassword.cs
+++ Venz.UI.Xaml.Controls/Source/UI/Xaml/ControlsExtensions/PasswordBox.UpdatablePassword.cs
@@ -7,18 +7,19 @@ namespace Venz.UI.Xaml.Controls
     {
         public static readonly DependencyProperty PasswordProperty =
             DependencyProperty.RegisterAttached("Password", typeof(String), typeof(PasswordBox),
-            new PropertyMetadata(Byte.MaxValue, (obj, args) => OnPasswordChanged((global::Windows.UI.Xaml.Controls.PasswordBox)obj, (String)args.NewValue)));
+            new PropertyMetadata(Byte.MaxValue, (obj, args) => OnPasswordChanged((global::Windows.UI.Xaml.Controls.PasswordBox)obj, args.OldValue, (String)args.NewValue)));
 
         public static String GetPassword(DependencyObject obj) => (String)obj.GetValue(PasswordProperty);
 
         public static void SetPassword(DependencyObject obj, String value) => obj.SetValue(PasswordProperty, value);
 
-        private static void OnPasswordChanged(global::Windows.UI.Xaml.Controls.PasswordBox element, String password)
+        private static void OnPasswordChanged(global::Windows.UI.Xaml.Controls.PasswordBox element, Object oldPassword, String password)
         {
             element.Unloaded -= OnElementUnloaded;
             element.Unloaded += OnElementUnloaded;
             element.PasswordChanged -= OnPasswordBoxPasswordChanged;
-            element.Password = password;
+            if (!(oldPassword is Byte))
+                element.Password = password ?? "";
             element.PasswordChanged += OnPasswordBoxPasswordChanged;
         }
 
