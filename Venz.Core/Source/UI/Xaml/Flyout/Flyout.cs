dc7943a8ed99ac694aaca7ac4828c2eab86660e8
Venzz <venexzz@gmail.com>
2018-08-08 13:23:31 +0300

    Added Flyout as a supported core object.

---
+++ Venz.Core/Source/UI/Xaml/Flyout/Flyout.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Windows.UI.Xaml;
+
+namespace Venz.UI.Xaml
+{
+    public class Flyout: global::Windows.UI.Xaml.Controls.Flyout
+    {
+        public Object DataContext { set => ((FrameworkElement)Content).DataContext = value; }
+
+        public Flyout()
+        {
+            Opened += (sender, args) => SetState();
+        }
+
+        protected virtual void SetState() { }
+    }
+}
