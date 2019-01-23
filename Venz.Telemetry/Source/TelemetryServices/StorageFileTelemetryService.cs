7bee164ebe554fc6527675da8986c0b3b4d842a5
Venzz <venexzz@gmail.com>
2019-01-23 12:45:54 +0300

    Reimplemented StorageFile telemetry service to reuse opened file stream instead of reopening it using FileIO.

--- Venz.Telemetry/Source/TelemetryServices/StorageFileTelemetryService.cs
+++ Venz.Telemetry/Source/TelemetryServices/StorageFileTelemetryService.cs
@@ -3,18 +3,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Storage;
+using Windows.Storage.Streams;
 
 namespace Venz.Telemetry
 {
     public sealed class StorageFileTelemetryService: ITelemetryService
     {
         private String FileName;
-        private Lazy<Task<StorageFile>> GetStorageFileTask;
+        private Lazy<Task<IRandomAccessStream>> GetLogFileStreamTask;
 
         public StorageFileTelemetryService(String fileName)
         {
             FileName = fileName;
-            GetStorageFileTask = new Lazy<Task<StorageFile>>(GetStorageFileAsync);
+            GetLogFileStreamTask = new Lazy<Task<IRandomAccessStream>>(GetLogFileStreamAsync);
         }
 
         public void Start() { }
@@ -25,57 +26,68 @@ namespace Venz.Telemetry
 
         public async void LogEvent(String title, String parameter, String value) => await LogEventAsync(title, parameter, value);
 
-        public async void LogException(String comment, Exception exception) => await LogExceptionAsync(comment, exception);
+        public async void LogException(String comment, Exception exception) => await LogExceptionInternalAsync(comment, exception);
 
         public IAsyncAction LogDailyEventAsync(String title) => TaskExtensions.CompletedAction;
 
         public IAsyncAction LogEventAsync(String title) => LogEventAsync(title, null, null);
 
-        public IAsyncAction LogEventAsync(String title, String parameter, String parameterValue)
-        {
-            if (title == null)
-                return TaskExtensions.CompletedAction;
+        public IAsyncAction LogEventAsync(String title, String parameter, String parameterValue) => LogEventInternalAsync(title, parameter, parameterValue).AsAsyncAction();
 
-            Func<Task> asyncAction = async () =>
+        private async Task LogEventInternalAsync(String title, String parameter, String parameterValue)
+        {
+            try
             {
-                try
-                {
-                    var storageFile = await GetStorageFileTask.Value.ConfigureAwait(false);
-                    var parameterString = (parameter == null) ? "" : $" >> {parameter} || {parameterValue}";
-                    await FileIO.AppendTextAsync(storageFile, $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} - {title}{parameterString}\n").AsTask().ConfigureAwait(false);
-                }
-                catch (Exception)
-                {
-                }
-            };
-            return Task.Run(asyncAction).AsAsyncAction();
+                if (title == null)
+                    return;
+
+                var logFileStream = await GetLogFileStreamTask.Value.ConfigureAwait(false);
+                var parameterString = (parameter == null) ? "" : $" >> {parameter} || {parameterValue}";
+                await AppendTextAsync(logFileStream, $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} - {title}{parameterString}\n").ConfigureAwait(false);
+            }
+            catch (Exception)
+            {
+            }
         }
 
-        private IAsyncAction LogExceptionAsync(String comment, Exception exception)
+        private async Task LogExceptionInternalAsync(String comment, Exception exception)
         {
-            Func<Task> asyncAction = async () =>
+            try
             {
-                try
-                {
-                    var storageFile = await GetStorageFileTask.Value.ConfigureAwait(false);
-                    var message = (comment != null) ? $"{comment}\n\n" : "";
-                    message += exception.AsDebugMessage();
-                    await FileIO.AppendTextAsync(storageFile, $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} - {message}\n").AsTask().ConfigureAwait(false);
-                }
-                catch (Exception)
-                {
-                }
-            };
-            return Task.Run(asyncAction).AsAsyncAction();
+                var logFileStream = await GetLogFileStreamTask.Value.ConfigureAwait(false);
+                var message = (comment != null) ? $"{comment}\n\n" : "";
+                message += exception.AsDebugMessage();
+                await AppendTextAsync(logFileStream, $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} - {message}\n").ConfigureAwait(false);
+            }
+            catch (Exception)
+            {
+            }
         }
 
 
 
-        private async Task<StorageFile> GetStorageFileAsync()
+        private async Task<IRandomAccessStream> GetLogFileStreamAsync()
         {
-            var storageFile = await GetStorageFileAsync(FileName).AsTask().ConfigureAwait(false);
-            await FileIO.AppendTextAsync(storageFile, "\n").AsTask().ConfigureAwait(false);
-            return storageFile;
+            var logFile = await GetStorageFileAsync(FileName).AsTask().ConfigureAwait(false);
+            var logStream = await logFile.OpenAsync(FileAccessMode.ReadWrite).AsTask().ConfigureAwait(false);
+            logStream.Seek(logStream.Size);
+            using (var dataWriter = new DataWriter(logStream))
+            {
+                dataWriter.WriteString("\n");
+                await dataWriter.StoreAsync().AsTask().ConfigureAwait(false);
+                dataWriter.DetachStream();
+            }
+            return logStream;
+        }
+
+        private static async Task AppendTextAsync(IRandomAccessStream logFileStream, String text)
+        {
+            using (var dataWriter = new DataWriter(logFileStream))
+            {
+                dataWriter.WriteString(text);
+                await dataWriter.StoreAsync().AsTask().ConfigureAwait(false);
+                dataWriter.DetachStream();
+            }
         }
 
         public static IAsyncOperation<StorageFile> GetStorageFileAsync(String fileName)
@@ -84,8 +96,9 @@ namespace Venz.Telemetry
             {
                 var fullFileName = $"{fileName} ({DateTime.Now.ToString("MMM, dd")}).log";
                 var diagnosticsFolder = await ApplicationData.Current.TemporaryFolder.CreateFolderAsync("Diagnostics", CreationCollisionOption.OpenIfExists).AsTask().ConfigureAwait(false);
-                var logFiles = (await diagnosticsFolder.GetFilesAsync().AsTask().ConfigureAwait(false)).Where(a => a.DisplayName.StartsWith($"{fileName} (")).OrderBy(a => a.DateCreated.DateTime).ToList();
-                while (logFiles.Count > 2)
+                var files = await diagnosticsFolder.GetFilesAsync().AsTask().ConfigureAwait(false);
+                var logFiles = files.Where(a => a.DisplayName.StartsWith($"{fileName} (")).OrderBy(a => a.DateCreated.DateTime).ToList();
+                while (logFiles.Count > 10)
                 {
                     await logFiles[0].DeleteAsync().AsTask().ConfigureAwait(false);
                     logFiles.RemoveAt(0);
